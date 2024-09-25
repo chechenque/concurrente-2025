b20b04dedbda6f5eebcf976789bc2414fb9fe7e0
laudima <laudima@ciencias.unam.mx>
2024-09-24 21:06:05 -0600

    Finish simulation

--- Practica3/src/main/java/kass/concurrente/modelos/Habitacion.java
+++ Practica3/src/main/java/kass/concurrente/modelos/Habitacion.java
@@ -1,23 +1,27 @@
 package kass.concurrente.modelos;
 
+import kass.concurrente.constantes.Constante;
+
 /**
  * Clase que fungira como habitacion
  * Se sabe que existe un interruptor que nos dice
  * si el foco esta prendido o no
  * Se desconoce el estado inicial del foco (Usar un random, para que sea
  * pseudoaleatorio el estado inicial)
- * @author <Equipo>
+ * @author Laura Itzel Rodriguez Dimayuga 
  * @version 1.0
  */
 public class Habitacion {
     private volatile boolean prendido;//Variable compartida
-
+    private Vocero vocero;
     /**
      * Metodo Constructor
      * Aqui se define el como estara el foco inicialmente
      */
     public Habitacion(){
-
+        // El boton de la luz empieza prendido o no de manera aleatoria
+        prendido = Math.random() < 0.5;
+        vocero = new Vocero(0, true, 0);
     }
 
     /**
@@ -31,6 +35,43 @@ public class Habitacion {
      * @throws InterruptedException Si falla algun hilo
      */
     public Boolean entraHabitacion(Prisionero prisionero) throws InterruptedException{
-        return null;
+        if(!prendido && prisionero.getMarcado() < 2 && !prisionero.getEsVocero()){
+            prendido = true;
+            prisionero.setMarcado(prisionero.getMarcado() + 1);
+            System.out.println(Constante.VERDE +"El prisionero " + prisionero.getId() + " ha prendido el interruptor" + Constante.RESET);
+        }
+        if(prisionero.getEsVocero()){
+            prisionero.setMarcado(prisionero.getMarcado() + 1);
+            vocero.setMarcado(vocero.getMarcado() + 1);
+            return entraHabitacion(vocero);
+        }
+        System.out.println(prisionero);
+        prisionero.setPaso(false);
+        return false; 
+    }
+
+    public Boolean entraHabitacion(Vocero vocero){
+        Boolean yaPasaron = false;
+        if(prendido){
+            prendido = false;
+            vocero.incrementaContador();
+        }
+        if(vocero.getContador() >= 2*Constante.PRISIONEROS - 2){
+            yaPasaron = true;
+        }
+        System.out.println(vocero);
+        return yaPasaron;
+    }
+
+    public void setPrendido(boolean prendido){
+        this.prendido = prendido;
+    }
+
+    public boolean getPrendido(){
+        return prendido;
+    }
+
+    public Vocero getVocero(){
+        return vocero;
     }
 }
--- Practica3/src/main/java/kass/concurrente/modelos/Vocero.java
+++ Practica3/src/main/java/kass/concurrente/modelos/Vocero.java
@@ -1,5 +1,7 @@
 package kass.concurrente.modelos;
 
+import kass.concurrente.constantes.Constante;
+
 /**
  * Este ess quien lleva la cuenta de los prisioneros que han entrado a la habitacion
  * a parte de los atributos de Prisionero, tambien posee un contador
@@ -8,10 +10,32 @@ package kass.concurrente.modelos;
  */
 public class Vocero extends Prisionero{
     protected Integer contador;
-    public Vocero(Integer id, Boolean esVocero, Boolean marcado) {
+
+    /**
+     * Metodo constructor 
+     * @param id
+     * @param esVocero
+     * @param marcado
+     */
+    public Vocero(Integer id, Boolean esVocero, int marcado) {
         super(id, esVocero, marcado);
-        //Completar y hacer documentacion
+        contador = 0;
     }
-    
-    //Mismo caso que el otro, annadir getter and setter
+
+    public Integer getContador() {
+        return contador;
+    }
+
+    public void setContador(Integer contador) {
+        this.contador = contador;
+    }
+
+    public void incrementaContador(){
+        contador++;
+    }
+    @Override
+    public String toString(){
+        return Constante.ROJO + "El prisionero " + id + " ha apagado el interruptor " + contador + " veces" + Constante.RESET;
+    }
 }
+
